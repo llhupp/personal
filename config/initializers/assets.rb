c7fd84f6f1b73ab5fd84c3ba277d7f7bf675e086
Lyric Hupp <lyric.hupp@code42.com>
2016-03-11 15:53:56 -0600

    Load less in landing page. Add band poster to Home page.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w(
-home/index.js photos/index.js events/index.js
+	landing.css landing.js home/index.js photos/index.js events/index.js
 )
 
 # Add additional assets to the asset load path
@@ -12,4 +12,3 @@ home/index.js photos/index.js events/index.js
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
